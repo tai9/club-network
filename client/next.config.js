c4e40d2289aee8e247242d6f9f8ddd905ba71c70
Tai <tantai.it95@gmail.com>
2023-12-08 10:35:42 +0700

    feat: config proxy

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8000/:path*", // Proxy to Backend
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
