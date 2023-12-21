3f306816fa73573f2eacfea7a447752abe69fb83
Tai <tantai.it95@gmail.com>
2023-12-21 12:19:34 +0700

    feat: share to X

--- client/src/components/home/PostsSection/Post.tsx
+++ client/src/components/home/PostsSection/Post.tsx
@@ -23,6 +23,7 @@ import { Button, Divider, Dropdown, Flex, Input, Modal, App } from "antd";
 import { useMemo, useState } from "react";
 import { Content, MoreLink, PostWrapper } from "./styled";
 import { useRouter } from "next/router";
+import { TwitterShareButton, TwitterIcon } from "react-share";
 
 type Props = {
   data: IPost;
@@ -53,6 +54,11 @@ const Post = ({ data }: Props) => {
   const isLiked = useMemo(
     () => data.reactions.findIndex((x) => x.memberId === memberData?.id) !== -1,
     [data.reactions, memberData?.id]
+  );
+
+  const postDetailLink = useMemo(
+    () => `${window.location.origin}/member/${memberData?.id}/post/${data.id}`,
+    [data.id, memberData?.id]
   );
 
   const { setOpenPostModal, setPostContent, setPost } = useClubNetwork();
@@ -132,9 +138,8 @@ const Post = ({ data }: Props) => {
   };
 
   const handleCopyLink = async () => {
-    const link = `${window.location.origin}/member/${memberData?.id}/post/${data.id}`;
-    message.success(`${link} copied to clipboard!`);
-    await navigator.clipboard.writeText(link);
+    message.success(`${postDetailLink} copied to clipboard!`);
+    await navigator.clipboard.writeText(postDetailLink);
   };
 
   const renderItems = () => {
@@ -241,14 +246,39 @@ const Post = ({ data }: Props) => {
 
       <div className="time">{formatLastTime(data.createdAt)}</div>
 
-      <Flex gap={24}>
-        <Flex gap={4} onClick={handleLike}>
-          {isLiked ? <HeartFilled /> : <HeartOutlined />}
-          <span>{likeCount || 0}</span>
+      <Flex gap={24} justify="space-between">
+        <Flex gap={24}>
+          <Flex gap={4} onClick={handleLike}>
+            {isLiked ? <HeartFilled /> : <HeartOutlined />}
+            <span>{likeCount || 0}</span>
+          </Flex>
+          <Flex gap={4} onClick={() => setToggleComments((prev) => !prev)}>
+            <MessageOutlined />
+            <span>{data.comments.length || 0}</span>
+          </Flex>
         </Flex>
-        <Flex gap={4} onClick={() => setToggleComments((prev) => !prev)}>
-          <MessageOutlined />
-          <span>{data.comments.length || 0}</span>
+        <Flex gap={16} align="center">
+          <div className="time">SHARE TO</div>
+          <TwitterShareButton
+            title={`${data.content} @Mr_Tai9`}
+            url={postDetailLink}
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <svg
+              stroke="currentColor"
+              fill="currentColor"
+              stroke-width="0"
+              viewBox="0 0 512 512"
+              height="16"
+              width="16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"></path>
+            </svg>
+          </TwitterShareButton>
         </Flex>
       </Flex>
 
