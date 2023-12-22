d938ed5895d610b4e73846d096ab274ae5bb6164
Tai <tantai.it95@gmail.com>
2023-12-22 11:14:34 +0700

    chore: update middleware to allow homepage

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -4,8 +4,8 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const memberId = req.cookies.get("memberId")?.value;
 
-  if (memberId && req.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL(`member/${memberId}`, req.url));
+  if (!memberId && req.nextUrl.pathname.includes("account")) {
+    return NextResponse.redirect(new URL(`/`, req.url));
   } else {
     return NextResponse.next();
   }
