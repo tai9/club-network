59feb9427da32c5fee7e6f252b62374be0c6e3c1
Tai <tantai.it95@gmail.com>
2023-12-25 23:03:37 +0700

    ui: login

--- client/src/components/common/AppModals/LoginModal.tsx
+++ client/src/components/common/AppModals/LoginModal.tsx
@@ -1,7 +1,15 @@
 import axiosClient from "@/configs/axiosClient";
 import useClubNetwork from "@/hooks/useClubNetwork";
 import { useMember } from "@/hooks/useMember";
-import { Button, Form, Input, Modal, Typography, App } from "antd";
+import {
+  AppleFilled,
+  FacebookFilled,
+  GoogleOutlined,
+  RightOutlined,
+  TwitterOutlined,
+  WalletOutlined,
+} from "@ant-design/icons";
+import { App, Button, Flex, Form, Input, Modal, Typography } from "antd";
 import { setCookie } from "cookies-next";
 
 type FieldType = {
@@ -51,48 +59,159 @@ const LoginModal = () => {
       onOk={onFinish}
       onCancel={onCancel}
       footer={null}
-      width={300}
+      width={500}
     >
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Typography.Title
-          level={4}
-          style={{
-            textAlign: "center",
-            marginTop: 20,
-            marginBottom: 24,
-          }}
-        >
-          Sign In
-        </Typography.Title>
-        <Form.Item<FieldType>
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
+      <Flex vertical gap={24}>
+        <Flex vertical align="center">
+          <Typography.Title
+            level={4}
+            style={{
+              marginTop: 20,
+            }}
+          >
+            Let&apos;s get started
+          </Typography.Title>
+          <Typography.Text>Choose your way</Typography.Text>
+        </Flex>
 
-        <Form.Item<FieldType>
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
+        <Flex vertical gap={8}>
+          <div>Get a password-free link to connect</div>
+          <Flex gap={12}>
+            <Input
+              style={{
+                height: 64,
+              }}
+              size="large"
+              placeholder="Enter your email"
+            />
+            <Button
+              style={{
+                height: 64,
+                width: 74,
+              }}
+              size="large"
+              type="primary"
+              icon={<RightOutlined />}
+            />
+          </Flex>
+        </Flex>
+        <Flex vertical gap={8}>
+          <div>Or connect with...</div>
+          <Flex gap={12}>
+            <Button
+              style={{
+                height: 64,
+                width: 64,
+              }}
+              size="large"
+              icon={
+                <GoogleOutlined
+                  style={{
+                    fontSize: 28,
+                  }}
+                />
+              }
+            />
+            <Button
+              style={{
+                height: 64,
+                width: 64,
+              }}
+              size="large"
+              icon={
+                <TwitterOutlined
+                  style={{
+                    fontSize: 28,
+                  }}
+                />
+              }
+            />
+            <Button
+              style={{
+                height: 64,
+                width: 64,
+              }}
+              size="large"
+              icon={
+                <FacebookFilled
+                  style={{
+                    fontSize: 28,
+                  }}
+                />
+              }
+            />
+            <Button
+              style={{
+                height: 64,
+                width: 64,
+              }}
+              size="large"
+              icon={
+                <AppleFilled
+                  style={{
+                    fontSize: 28,
+                  }}
+                />
+              }
+            />
+            <Button
+              style={{
+                height: 64,
+                flexGrow: 1,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              size="large"
+              icon={
+                <WalletOutlined
+                  style={{
+                    fontSize: 28,
+                  }}
+                />
+              }
+            >
+              Wallet
+            </Button>
+          </Flex>
+        </Flex>
+        <Flex vertical gap={12}>
+          <div>Or login with...</div>
+          <Form
+            name="basic"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            layout="vertical"
+          >
+            <Form.Item<FieldType>
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input size="large" />
+            </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Sign In
-          </Button>
-        </Form.Item>
-      </Form>
+            <Form.Item<FieldType>
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password size="large" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button size="large" type="primary" htmlType="submit">
+                JOIN
+              </Button>
+            </Form.Item>
+          </Form>
+        </Flex>
+      </Flex>
     </Modal>
   );
 };
