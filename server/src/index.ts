0dfd81202108b1e114ef88de90ca3e241cae4a8f
Tai <tantai.it95@gmail.com>
2024-02-29 21:08:26 +0700

    fix: home route

--- server/src/index.ts
+++ server/src/index.ts
@@ -51,7 +51,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Club Network API");
 });
 
