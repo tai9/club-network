d545d9c3c74c51af09fcd22fa13253ae7bbb5edf
Tai <tantai.it95@gmail.com>
2024-03-13 18:40:31 +0700

    chore: update redis config

--- server/src/configs/redis.config.ts
+++ server/src/configs/redis.config.ts
@@ -1,14 +1,15 @@
 import Redis from "ioredis";
 
 export const redisClient = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
+  host: process.env.REDIS_HOST || "127.0.0.2",
   port: +process.env.REDIS_PORT || 6379,
   username: process.env.REDIS_USERNAME || "default",
   password: process.env.REDIS_PASSWORD || "",
   keyPrefix: process.env.APP_NAME,
+  tls: {},
 });
 
-redisClient.on("connect", () => {
+redisClient.on("connection", () => {
   if (process.env.NODE_ENV !== "test") {
     console.log("Redis client connected");
   }
