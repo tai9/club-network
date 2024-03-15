b76ba6bdd10cf3c3a3361f778dfc1c7b64fb5c17
Tai <tantai.it95@gmail.com>
2024-03-15 12:49:53 +0700

    build: debug 14

--- server/src/configs/contract.config.ts
+++ server/src/configs/contract.config.ts
@@ -1,8 +1,10 @@
+import { Sepolia } from "@thirdweb-dev/chains";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import "dotenv/config";
 
 export const sdk = ThirdwebSDK.fromPrivateKey(
   process.env.WALLET_PRIVATE_KEY,
-  "sepolia",
+  Sepolia,
   {
     secretKey: process.env.CONTRACT_SECRET_KEY,
   }
