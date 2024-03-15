ea7d529049b5ac234e7702b3cccf62e015cdf946
Tai <tantai.it95@gmail.com>
2024-03-15 14:08:22 +0700

    build: debug 21

--- server/src/controllers/ticket.controller.ts
+++ server/src/controllers/ticket.controller.ts
@@ -7,7 +7,7 @@ import { constants } from "http2";
 import Joi from "joi";
 import sharp from "sharp";
 // import { Sepolia } from "@thirdweb-dev/chains";
-// import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
 const getTicketsSchema = Joi.object<IGetTicketsParams>({
   search: Joi.string().optional(),
